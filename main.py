98ce79a7a097d7f651635cd974d99bae05905834
Orkhan Aliyev <006807@ibar.int>
2024-08-10 11:26:22 +0400

    added

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
+from fastapi.responses import HTMLResponse  # Import HTMLResponse
 
 app = FastAPI()
+
+# Serve static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Set up templates directory
 templates = Jinja2Templates(directory="templates")
